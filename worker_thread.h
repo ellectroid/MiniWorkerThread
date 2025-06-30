66182efaef8df59d6c3d676ff3e012f39906c392
Ilya <72581491+ellectroid@users.noreply.github.com>
2025-06-30 14:31:58 +0200

    added atomic flags, fixed cleanup, added reset, fixed instant detach for reuse

--- worker_thread.h
+++ worker_thread.h
@@ -1,6 +1,7 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 
 class WorkerThread {
 public:
@@ -26,10 +27,8 @@ private:
 	std::condition_variable m_cond;
 	int m_signal;
 	work_handlerfunc_t m_workFunc;
-	unsigned int m_flags;
-
-	// Embedded work data
-	void *m_work_arg_ptr[2];
+	std::atomic<unsigned int> m_flags;
+	void* m_work_arg_ptr[2];
 	union {
 		unsigned long long m_work_arg_uint[2];
 		long long m_work_arg_int[2];
@@ -37,11 +36,15 @@ private:
 
 public:
 	WorkerThread() :
-			m_signal(SIGNAL_NONE), m_workFunc(nullptr), m_flags(0) {
+			m_thread(),
+			m_mutex(),
+			m_cond(),
+			m_signal(SIGNAL_NONE),
+			m_workFunc(nullptr),
+			m_flags(0) {
 		for (size_t i = 0;
 				i < sizeof(m_work_arg_ptr) / sizeof(m_work_arg_ptr[0]); ++i)
 			m_work_arg_ptr[i] = nullptr;
-
 		for (size_t i = 0;
 				i < sizeof(m_work_arg_int) / sizeof(m_work_arg_int[0]); ++i)
 			m_work_arg_int[i] = 0;
@@ -50,13 +53,12 @@ public:
 	~WorkerThread() {
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
-			m_flags |= FLAG_TERMINATE_PENDING;
+			m_flags.fetch_or(FLAG_TERMINATE_PENDING);
 			m_signal = SIGNAL_KILL;
 		}
 		m_cond.notify_one();
-
 		if (m_thread.joinable()) {
-			if (m_flags & FLAG_DETACH_ON_TERMINATE)
+			if (m_flags.load() & FLAG_DETACH_ON_TERMINATE)
 				m_thread.detach();
 			else
 				m_thread.join();
@@ -65,15 +67,13 @@ public:
 
 	void start() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-
 		if (m_thread.joinable()) {
-			if (m_flags & FLAG_DETACH_ON_TERMINATE)
+			if (m_flags.load() & FLAG_DETACH_ON_TERMINATE)
 				m_thread.detach();
 			else
 				m_thread.join();
 		}
-
-		if (!(m_flags & FLAG_THREAD_ACTIVE)) {
+		if (!(m_flags.load() & FLAG_THREAD_ACTIVE)) {
 			m_thread = std::thread(&WorkerThread::threadLoop, this);
 		}
 	}
@@ -81,16 +81,19 @@ public:
 	void stop_detach() {
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
-			m_flags |= FLAG_DETACH_ON_TERMINATE;
+			m_flags.fetch_or(FLAG_DETACH_ON_TERMINATE);
 			m_signal = SIGNAL_KILL;
 		}
 		m_cond.notify_one();
+		if ((m_flags.load() & FLAG_THREAD_ACTIVE) && m_thread.joinable()) {
+			m_thread.detach();
+		}
 	}
 
 	void stop_join() {
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
-			m_flags &= ~FLAG_DETACH_ON_TERMINATE;
+			m_flags.fetch_and(~FLAG_DETACH_ON_TERMINATE);
 			m_signal = SIGNAL_KILL;
 		}
 		m_cond.notify_one();
@@ -99,7 +102,7 @@ public:
 	void requestWork() {
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
-			m_flags |= FLAG_WORK_PENDING;
+			m_flags.fetch_or(FLAG_WORK_PENDING);
 		}
 		m_cond.notify_one();
 	}
@@ -111,68 +114,56 @@ public:
 
 	void enableWorkRepeat() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		m_flags |= FLAG_WORK_REPEAT;
+		m_flags.fetch_or(FLAG_WORK_REPEAT);
 	}
 
 	void disableWorkRepeat() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		m_flags &= ~FLAG_WORK_REPEAT;
+		m_flags.fetch_and(~FLAG_WORK_REPEAT);
 	}
 
-	void setWorkPointerArg(int i, void *ptr) {
+	void setWorkPointerArg(int i, void* ptr) {
 		if (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_ptr)
-								/ sizeof(m_work_arg_ptr[0])))
+				&& i < static_cast<int>(sizeof(m_work_arg_ptr) / sizeof(m_work_arg_ptr[0])))
 			m_work_arg_ptr[i] = ptr;
 	}
 
 	void* getWorkPointerArg(int i) const {
 		return (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_ptr)
-								/ sizeof(m_work_arg_ptr[0]))) ?
-				m_work_arg_ptr[i] : nullptr;
+				&& i < static_cast<int>(sizeof(m_work_arg_ptr) / sizeof(m_work_arg_ptr[0])))
+				? m_work_arg_ptr[i] : nullptr;
 	}
 
 	void setWorkUIntArg(int i, unsigned long long val) {
 		if (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_uint)
-								/ sizeof(m_work_arg_uint[0])))
+				&& i < static_cast<int>(sizeof(m_work_arg_uint) / sizeof(m_work_arg_uint[0])))
 			m_work_arg_uint[i] = val;
 	}
 
 	unsigned long long getWorkUIntArg(int i) const {
 		return (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_uint)
-								/ sizeof(m_work_arg_uint[0]))) ?
-				m_work_arg_uint[i] : 0;
+				&& i < static_cast<int>(sizeof(m_work_arg_uint) / sizeof(m_work_arg_uint[0])))
+				? m_work_arg_uint[i] : 0;
 	}
 
 	void setWorkIntArg(int i, long long val) {
 		if (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_int)
-								/ sizeof(m_work_arg_int[0])))
+				&& i < static_cast<int>(sizeof(m_work_arg_int) / sizeof(m_work_arg_int[0])))
 			m_work_arg_int[i] = val;
 	}
 
 	long long getWorkIntArg(int i) const {
 		return (i >= 0
-				&& i
-						< static_cast<int>(sizeof(m_work_arg_int)
-								/ sizeof(m_work_arg_int[0]))) ?
-				m_work_arg_int[i] : 0;
+				&& i < static_cast<int>(sizeof(m_work_arg_int) / sizeof(m_work_arg_int[0])))
+				? m_work_arg_int[i] : 0;
 	}
 
 	void setDetachOnTerminate(bool enable) {
 		std::lock_guard<std::mutex> lock(m_mutex);
 		if (enable)
-			m_flags |= FLAG_DETACH_ON_TERMINATE;
+			m_flags.fetch_or(FLAG_DETACH_ON_TERMINATE);
 		else
-			m_flags &= ~FLAG_DETACH_ON_TERMINATE;
+			m_flags.fetch_and(~FLAG_DETACH_ON_TERMINATE);
 	}
 
 	void sendSignal(int signal) {
@@ -190,28 +181,40 @@ public:
 
 	unsigned int getFlags() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		return m_flags;
+		return m_flags.load();
 	}
 
 	bool isIdle() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		return (m_flags & FLAG_IDLE) != 0;
+		return (m_flags.load() & FLAG_IDLE) != 0;
 	}
 
 	bool isThreadActive() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		return (m_flags & FLAG_THREAD_ACTIVE) != 0;
+		return (m_flags.load() & FLAG_THREAD_ACTIVE) != 0;
 	}
 
 	bool isBusy() {
 		std::lock_guard<std::mutex> lock(m_mutex);
-		return (m_flags & FLAG_BUSY) != 0;
+		return (m_flags.load() & FLAG_BUSY) != 0;
+	}
+
+	void reset() {
+		stop_detach();
+		std::lock_guard<std::mutex> lock(m_mutex);
+		m_signal = SIGNAL_NONE;
+		m_workFunc = nullptr;
+		m_flags.store(0);
+		for (size_t i = 0; i < sizeof(m_work_arg_ptr) / sizeof(m_work_arg_ptr[0]); ++i)
+			m_work_arg_ptr[i] = nullptr;
+		for (size_t i = 0; i < sizeof(m_work_arg_int) / sizeof(m_work_arg_int[0]); ++i)
+			m_work_arg_int[i] = 0;
 	}
 
 private:
 	void handleSignal() {
 		if (m_signal == SIGNAL_KILL) {
-			m_flags |= FLAG_TERMINATE_PENDING;
+			m_flags.fetch_or(FLAG_TERMINATE_PENDING);
 		}
 		m_signal = SIGNAL_NONE;
 	}
@@ -219,42 +222,43 @@ private:
 	void threadLoop() {
 		{
 			std::lock_guard<std::mutex> lock(m_mutex);
-			m_flags |= FLAG_THREAD_ACTIVE;
+			m_flags.fetch_or(FLAG_THREAD_ACTIVE);
 		}
 
 		while (true) {
 			{
 				std::unique_lock<std::mutex> lock(m_mutex);
-				m_flags |= FLAG_IDLE;
+				m_flags.fetch_or(FLAG_IDLE);
 
 				m_cond.wait(lock,
 						[this] {
-							return (m_flags & FLAG_WORK_PENDING)
+							return (m_flags.load() & FLAG_WORK_PENDING)
 									|| (m_signal != SIGNAL_NONE);
 						});
 
-				m_flags &= ~FLAG_IDLE;
+				m_flags.fetch_and(~FLAG_IDLE);
 
 				handleSignal();
-				if (m_flags & FLAG_TERMINATE_PENDING)
+				if (m_flags.load() & FLAG_TERMINATE_PENDING)
 					break;
 
-				if ((m_flags & FLAG_WORK_PENDING) && m_workFunc) {
-					if (!(m_flags & FLAG_WORK_REPEAT))
-						m_flags &= ~FLAG_WORK_PENDING;
+				if ((m_flags.load() & FLAG_WORK_PENDING) && m_workFunc) {
+					if (!(m_flags.load() & FLAG_WORK_REPEAT))
+						m_flags.fetch_and(~FLAG_WORK_PENDING);
 
-					m_flags |= FLAG_BUSY;
+					m_flags.fetch_or(FLAG_BUSY);
 					lock.unlock();
 					m_workFunc(this);
 					lock.lock();
-					m_flags &= ~FLAG_BUSY;
+					m_flags.fetch_and(~FLAG_BUSY);
 				}
 			}
 		}
 
 		std::lock_guard<std::mutex> lock(m_mutex);
-		m_flags &= ~FLAG_TERMINATE_PENDING;
-		m_flags &= ~FLAG_WORK_REPEAT;
-		m_flags &= ~FLAG_THREAD_ACTIVE; // Reset last for state polling guarantees
+		m_flags.fetch_and(~FLAG_TERMINATE_PENDING);
+		m_flags.fetch_and(~FLAG_WORK_REPEAT);
+		m_flags.fetch_and(~FLAG_WORK_PENDING);
+		m_flags.fetch_and(~FLAG_THREAD_ACTIVE);
 	}
 };
